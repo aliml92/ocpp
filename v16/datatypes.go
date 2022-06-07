15485ddf483a1cbc7ac6154793133e1f7ca70e7d
aliml92 <almowins5yearsss@gmail.com>
2022-06-07 17:10:46 +0900

    idTag fixed

--- v16/datatypes.go
+++ v16/datatypes.go
@@ -3,7 +3,7 @@ package v16
 
 
 type IdTagInfo struct {
-	ExpiryDate   string              `json:"expiryDate,omitempty" validate:"ISO8601date"`
+	ExpiryDate   string              `json:"expiryDate,omitempty" validate:"omitempty,ISO8601date"`
 	ParentIdTag  string              `json:"parentIdTag,omitempty" validate:"max=20"`
 	Status       string              `json:"status" validate:"required,AuthorizationStatus"`
 }
